3ebd5bd2435d4a5620a220d871677ab6c39a5456
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2017-03-10 17:20:11 +0700

    Reverse items order in LogActivity Add total in log activity Add action for FAB in LogActivity

--- app/src/main/java/ru/jdev/q5/LogActivity.kt
+++ app/src/main/java/ru/jdev/q5/LogActivity.kt
@@ -2,6 +2,7 @@ package ru.jdev.q5
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Gravity
@@ -10,6 +11,8 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import org.jetbrains.anko.find
+import org.jetbrains.anko.onClick
+import ru.jdev.q5.R.string.sum
 import java.text.SimpleDateFormat
 
 
@@ -32,46 +35,62 @@ class LogActivity : AppCompatActivity() {
         table.setColumnShrinkable(2, true)
         table.setColumnStretchable(2, true)
         updateTable()
+
+        find<FloatingActionButton>(R.id.fab).onClick {
+            val configIntent = Intent(this, EnterSumActivity::class.java)
+            configIntent.putExtra(EnterSumActivity.sourceExtra, "manual")
+            startActivity(configIntent)
+        }
     }
 
     private fun updateTable() {
         val table = find<TableLayout>(R.id.table)
+
         table.removeAllViews()
-        log.list().forEach {
-            val dateLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
-            dateLayoutParams.rightMargin = 40
-            dateLayoutParams.weight = 1.0F
-
-            val sumLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
-            sumLayoutParams.rightMargin = 20
-            sumLayoutParams.weight = 2.0F
-
-            val categoryLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
-            categoryLayoutParams.gravity = Gravity.CENTER
-            categoryLayoutParams.weight = 2.0F
-
-            val tableRow = TableRow(this)
-            tableRow.layoutParams = tableParams// TableLayout is the parent view
-
-            val date = TextView(this)
-            date.gravity = Gravity.CENTER
-            date.layoutParams = dateLayoutParams// TableRow is the parent view
-            date.text = dateFormat.format(it.date.dateTime)
-            tableRow.addView(date)
-
-            val sum = TextView(this)
-            sum.gravity = Gravity.LEFT
-            sum.layoutParams = sumLayoutParams// TableRow is the parent view
-            sum.text = it.sum
-            tableRow.addView(sum)
-
-            val category = TextView(this)
-            category.layoutParams = categoryLayoutParams// TableRow is the parent view
-            category.text = it.category
-            tableRow.addView(category)
-
-            table.addView(tableRow)
+        val trxes = log.list().toList().reversed()
+        trxes.forEach {
+            val row = createRow(dateFormat.format(it.date.dateTime), it.sum, it.category)
+            table.addView(row)
         }
+
+        val row = createRow("", trxes.sumByDouble { it.sum.replace(',', '.').toDouble() }.toString().replace('.', ','), "Итого")
+        table.addView(row)
+    }
+
+    private fun createRow(date: String, sum: String, category: String): TableRow {
+        val dateLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+        dateLayoutParams.rightMargin = 40
+        dateLayoutParams.weight = 1.0F
+
+        val sumLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+        sumLayoutParams.rightMargin = 20
+        sumLayoutParams.weight = 2.0F
+
+        val categoryLayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+        categoryLayoutParams.gravity = Gravity.CENTER
+        categoryLayoutParams.weight = 2.0F
+
+        val tableRow = TableRow(this)
+        tableRow.layoutParams = tableParams// TableLayout is the parent view
+
+        val dateView = TextView(this)
+        dateView.gravity = Gravity.CENTER
+        dateView.layoutParams = dateLayoutParams// TableRow is the parent view
+        dateView.text = date
+        tableRow.addView(dateView)
+
+        val sumView = TextView(this)
+        sumView.gravity = Gravity.LEFT
+        sumView.layoutParams = sumLayoutParams// TableRow is the parent view
+        sumView.text = sum
+        tableRow.addView(sumView)
+
+        val categoryView = TextView(this)
+        categoryView.layoutParams = categoryLayoutParams// TableRow is the parent view
+        categoryView.text = category
+        tableRow.addView(categoryView)
+
+        return tableRow
     }
 
     override fun onActivityReenter(resultCode: Int, data: Intent?) {
