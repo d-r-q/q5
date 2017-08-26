0d42de2e762fa0fdc1ec9319fcfc62da46132b25
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2017-08-26 14:35:01 +0700

    Add full transaction parsing from csv v1 and v2

--- app/src/main/java/ru/jdev/q5/Transaction.kt
+++ app/src/main/java/ru/jdev/q5/Transaction.kt
@@ -11,18 +11,35 @@ import java.util.*
 data class Transaction(val date: TrxDate, val sum: String, val category: String, val comment: String, val device: String, val source: String) : Serializable {
 
     companion object {
+
+        val notParsed = "not parsed"
+
         private val delimiter = ';'
+
         fun parse(line: String): Transaction {
 
-            val fields = line.split("\"$delimiter\"").map { it.trim('\"') }
+            val fieldsV1 = line
+                    .replace("\uFEFF", "") // удаление BOM-ов
+                    .split("\",\"".toRegex())
+                    .map { it.trim('\"') }
+            val fieldsV2 = line
+                    .replace("\uFEFF", "") // удаление BOM-ов
+                    .split("\";\"".toRegex())
+                    .map { it.trim('\"') }
+            val v1 = fieldsV1.size > fieldsV2.size
+            val fields = if (v1) fieldsV1 else fieldsV2
+
             val date = if (fields.size > 0) fields[0] else "00.00.00"
             val time = if (fields.size > 1) fields[1] else "00:00"
-            val sum = if (fields.size > 2) fields[2] else "not parsed"
-            val cat = if (fields.size > 3) fields[3] else "not parsed"
+            val sum = if (fields.size > 2) fields[2] else notParsed
+            val cat = if (fields.size > 3) fields[3] else notParsed
+            val comment = if (fields.size > 4) fields[4].replace("\"\"", "\"") else notParsed
+            val device = if (fields.size > 5) fields[5] else notParsed
+            val source = if (fields.size > 6) fields[6] else notParsed
             return try {
-                Transaction(TrxDate(date, time), sum, cat, "TODO", "TODO", "TODO")
+                Transaction(if (v1) TrxDate.parseV1(date, time) else TrxDate(date, time), sum, cat, comment, device, source)
             } catch (e: ParseException) {
-                Transaction(TrxDate("00.00.00", "00:00"), "0", "Not Parsed", "Not Parsed", "Not parsed", "NotParsed")
+                Transaction(TrxDate("00.00.00", "00:00"), "0", notParsed, notParsed, notParsed, notParsed)
             }
         }
     }
@@ -39,11 +56,18 @@ data class Transaction(val date: TrxDate, val sum: String, val category: String,
 class TrxDate(val dateTime: Date) : Serializable {
 
     companion object {
+        val datePatternV1 = "yy.MM.dd"
+
         val datePattern = "dd.MM.yyyy"
         val timePattern = "HH:mm"
+
+        val dateTimeFormatV1 = SimpleDateFormat("$datePatternV1 $timePattern")
+
         private val dateFormat = SimpleDateFormat(datePattern)
         private val timeFormat = SimpleDateFormat(timePattern)
         private val dateTimeFormat = SimpleDateFormat("$datePattern $timePattern")
+
+        fun parseV1(date: String, time: String) = TrxDate(dateTimeFormatV1.parse("$date $time"))
 
         fun isValidDate(date: String) = dateFormat.matches(date)
         fun isValidTime(time: String) = timeFormat.matches(time)
---
+++ app/src/test/java/ru/jdev/q5/TransactionsParsingTest.kt
@@ -0,0 +1,38 @@
+package ru.jdev.q5
+
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+
+class TransactionsParsingTest {
+
+    private val months = listOf(2, 3, 4, 5, 6, 7, 8)
+    private val devices = listOf("F5321", "OnePlus3")
+
+    @Test
+    fun testTransactionsParsing() {
+        devices.forEach { device ->
+            months.forEach { month ->
+                val lines = javaClass.getResourceAsStream("/transaction_parsing/170$month-$device-v2.csv")?.bufferedReader()?.lineSequence()
+                lines
+                        ?.filter { it.trim().isNotEmpty() }
+                        ?.forEach { it ->
+                            val t = Transaction.parse(it)
+                            if (it.contains("\",\"")) {
+                                val c = Calendar.getInstance()
+                                c.time = t.date.dateTime
+                                Assert.assertEquals(it, 2017, c.get(Calendar.YEAR))
+                                Assert.assertEquals(it, month - 1, c.get(Calendar.MONTH))
+                                Assert.assertNotEquals(Transaction.notParsed, t.sum)
+                                Assert.assertNotEquals(Transaction.notParsed, t.category)
+                                Assert.assertNotEquals(Transaction.notParsed, t.comment)
+                                Assert.assertNotEquals(Transaction.notParsed, t.device)
+                                Assert.assertNotEquals(Transaction.notParsed, t.source)
+                            } else {
+                                Assert.assertEquals(it.replace("\uFEFF", ""), t.toCsvLine())
+                            }
+                        }
+            }
+        }
+    }
+}
